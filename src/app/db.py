9832b48bb143485c57e814d67bf22229fed73068
KenMwaura1 <kemwaura@gmail.com>
2025-01-31 15:19:59 +0300

    Refactor notes table definition and update Pydantic models for better schema representation

--- src/app/db.py
+++ src/app/db.py
@@ -15,25 +15,27 @@ DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://hello_fastapi:hello_fasta
 # SQLAlchemy
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
-# notes = Table(
-#     "notes",
-#     metadata,
-#     Column("id", Integer, primary_key=True),
-#     Column("title", String(50)),
-#     Column("description", String(50)),
-#     Column("completed",String(8), default="False"),
-#     Column("created_date", String(50), default=dt.now(tz("Africa/Nairobi")).strftime("%Y-%m-%d %H:%M"))
-# )
+notes = Table(
+    "notes",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(50)),
+    Column("description", String(50)),
+    Column("completed",String(8), default="False"),
+    Column("created_date", String(50), default=dt.now(tz("Africa/Nairobi")).strftime("%Y-%m-%d %H:%M"))
+)
 # # Databases query builder
 
 database = Database(DATABASE_URL)
 
-class notes(BaseModel):
-    id: int
+class NoteSchema(BaseModel):
     title: str
     description: str
     completed: bool
-    created_date: str = dt.now(tz("Africa/Nairobi")).strftime("%Y-%m-%d %H:%M")
+
+class NoteDB(NoteSchema):
+    id: int
+    created_date: str
 
     class Config:
         from_attributes = True
