cfad3d8fc4c6f4a2a7ac623df68ec46ee6818bf4
KenMwaura1 <kemwaura@gmail.com>
2025-01-31 14:43:47 +0300

    Add Pydantic model for notes and refactor database table definition

--- src/app/db.py
+++ src/app/db.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from databases import Database
 from datetime import datetime as dt
 from pytz import timezone as tz
+from pydantic import BaseModel
+
 
 load_dotenv()
 # Database url if none is passed the default one is used
@@ -13,15 +15,35 @@ DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://hello_fastapi:hello_fasta
 # SQLAlchemy
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
-notes = Table(
-    "notes",
-    metadata,
-    Column("id", Integer, primary_key=True),
-    Column("title", String(50)),
-    Column("description", String(50)),
-    Column("completed",String(8), default="False"),
-    Column("created_date", String(50), default=dt.now(tz("Africa/Nairobi")).strftime("%Y-%m-%d %H:%M"))
-)
-# Databases query builder
+# notes = Table(
+#     "notes",
+#     metadata,
+#     Column("id", Integer, primary_key=True),
+#     Column("title", String(50)),
+#     Column("description", String(50)),
+#     Column("completed",String(8), default="False"),
+#     Column("created_date", String(50), default=dt.now(tz("Africa/Nairobi")).strftime("%Y-%m-%d %H:%M"))
+# )
+# # Databases query builder
 
 database = Database(DATABASE_URL)
+
+class notes(BaseModel):
+    id: int
+    title: str
+    description: str
+    completed: bool
+    created_date: str = dt.now(tz("Africa/Nairobi")).strftime("%Y-%m-%d %H:%M")
+
+    class Config:
+        from_attributes = True
+        arbitrary_types_allowed = True
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "title": "My first note",
+                "description": "This is my first note",
+                "completed": False,
+                "created_date": "2021-07-01 00:00:00"
+            }
+        }
